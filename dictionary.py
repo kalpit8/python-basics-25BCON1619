8e32e36d6916163c09ce0d89f5fcecd4e9f5b095
kalpit8 <malhotrakalpit8@gmail.com>
2026-09-17 09:20:36 +0530

    doc add comments in the program

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,4 @@
-dict = {"Name": "Rahul", "Roll": 101, "Marks": 87.5}
+dict = {"Name": "Rahul", "Roll": 101, "Marks": 87.5} # dictionary program 
 
 for key in dict:
-    print(f"{key.capitalize()}: {dict[key]}")
+    print(f"{key.capitalize()}: {dict[key]}") # print dictionary 
